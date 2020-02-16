01d1d1a9b6fd7d7c209c8d63edc1506139a8d89f
Joao Pedro <jlourenco@alunos.utfpr.edu.br>
2020-02-16 17:09:31 -0300

    Update fix

--- src/app/pages/dados/dados.page.ts
+++ src/app/pages/dados/dados.page.ts
@@ -15,93 +15,95 @@ import { AngularFireStorage } from '@angular/fire/storage';
 export class DadosPage implements OnInit {
 
   public user: any;
-  public userDados: User ={};
-  
+  public userDados: User = {};
+
   public password: any = null;
   private loading: any;
   public passwordType: string = "password";
   passwordShow: boolean = false;
   public uploadPercent: Observable<number>;
   public URL: Observable<string>;
-  private blob:Blob;
+  private blob: Blob;
   constructor(public FirebaseAuth: AngularFireAuth,
-              private loadingCtrl: LoadingController,
-              private toastCtrl: ToastController,
-              private camera: Camera,
-              private file: File, 
-              private Storage: AngularFireStorage,) { }
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController,
+    private camera: Camera,
+    private file: File,
+    private Storage: AngularFireStorage, ) { }
 
   ngOnInit() {
-  this.user = this.FirebaseAuth.auth.currentUser
-  this.userDados = Object.assign({},this.user)
+    this.user = this.FirebaseAuth.auth.currentUser
+    this.userDados = Object.assign({}, this.user)
   }
 
-  async fotos(){
+  async fotos() {
     const options: CameraOptions = {
       quality: 70,
       destinationType: this.camera.DestinationType.FILE_URI,
       sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
       correctOrientation: true,
-      saveToPhotoAlbum:false,
+      saveToPhotoAlbum: false,
     }
-    try{
-      
-      
+    try {
+
+
       const fileUri: string = await this.camera.getPicture(options);
 
 
-      let file: string = fileUri.substring(fileUri.lastIndexOf('/')+1,fileUri.indexOf('?'));
-  
-      
-      const path: string = fileUri.substring(0,fileUri.lastIndexOf('/'));
+      let file: string = fileUri.substring(fileUri.lastIndexOf('/') + 1, fileUri.indexOf('?'));
+
+
+      const path: string = fileUri.substring(0, fileUri.lastIndexOf('/'));
 
 
       const buffer: ArrayBuffer = await this.file.readAsArrayBuffer(path, file);
 
-      const blob = new Blob([buffer], {type: 'image/jpeg'});
+      const blob = new Blob([buffer], { type: 'image/jpeg' });
 
       this.uploadPicture(blob);
-      
-    }catch(error){
+
+    } catch (error) {
       console.log(error)
     }
   }
 
 
 
- async uploadPicture(blob:Blob){
+  async uploadPicture(blob: Blob) {
 
-    const ref =  this.Storage.ref(this.user.uid + '.jpg');
+    const ref = this.Storage.ref(this.user.uid + '.jpg');
 
     const task = ref.put(blob);
 
     this.uploadPercent = task.percentageChanges();
 
-   task.snapshotChanges().pipe(
-      finalize( ()=> ref.getDownloadURL().subscribe( async (x) => { this.userDados.photoURL = await x;}))
-       
+    task.snapshotChanges().pipe(
+      finalize(() => ref.getDownloadURL().subscribe(async (x) => { this.userDados.photoURL = await x; }))
+
     ).subscribe();
 
-}
-  
-  
-  async salvar(){
- 
-    this.user.updateProfile({
-        displayName: this.userDados.displayName,
-        photoURL: this.userDados.photoURL,
-      }).then(function() {
-        // Update successful.
-      }).catch(function(error) {
-        // An error happened.
-      });
+  }
 
-      
-    
-    this.user.updateEmail(this.userDados.email).then(function() {
+
+  async salvar() {
+
+    var msg: string;
+
+    await this.user.updateProfile({
+      displayName: this.userDados.displayName,
+      photoURL: this.userDados.photoURL,
+    }).then(function () {
+      msg = "Salvo com sucesso!"
+    }).catch(function (error) {
+      msg = "ERRO"
+    });
+
+
+
+    await this.user.updateEmail(this.userDados.email).then(function () {
       // Update successful.
-    }).catch(function(error) {
-      let message: string='ERRO'; 
+    }).catch(function (error) {
+      let message: string = 'ERRO';
       switch (error.code) {
         case 'auth/email-already-in-use':
           message = 'E-mail já está sendo usado!';
@@ -111,18 +113,17 @@ export class DadosPage implements OnInit {
           break;
         case 'auth/requires-recent-login':
           message = 'Para alterar o seu email, você precisa ter feito login recentemente.';
-          break; 
+          break;
       }
-      console.log(error.message)
-      alert(error.message);
+      msg = message;
     });
 
-    if((this.password)!=null) {
+    if ((this.password) != null) {
 
-      this.user.updatePassword(this.password).then(function() {
+      await this.user.updatePassword(this.password).then(function () {
         // Update successful.
-      }).catch(function(error) {
-        let message: string='ERRO'; 
+      }).catch(function (error) {
+        let message: string = 'ERRO';
         switch (error.code) {
           case 'auth/weak-password':
             message = 'Senha fraca';
@@ -131,34 +132,48 @@ export class DadosPage implements OnInit {
             message = 'Para alterar a sua senha, você precisa ter feito login recentemente.';
             break;
         }
-        console.log(error.message);
-        alert(error.message);
-        }); 
+        msg = message;
+      });
     }
-   }
-
-
-  excluir(){
-    this.user.delete().then(function() {
-      // User deleted.
-    }).catch(function(error) {
-      console.log(error.code)
-    });
+    this.presentToast(msg);
   }
 
-  public togglePassword(){
-    if(this.passwordShow){
+
+  async excluir() {
+    var msg: string;
+
+    await this.user.delete().then(function () {
+      // User deleted.
+    }).catch(function (error) {
+      let message: string = 'ERRO';
+      switch (error.code) {
+        case 'auth/requires-recent-login':
+          message = 'Para excluir sua conta, você precisa ter feito login recentemente.';
+          break;
+      }
+      msg = message;
+    });
+    this.presentToast(msg);
+  }
+
+  public togglePassword() {
+    if (this.passwordShow) {
       this.passwordShow = false;
       this.passwordType = "password";
-    }else{
+    } else {
       this.passwordShow = true;
       this.passwordType = "text";
     }
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({ message, duration: 2000 });
+    toast.present();
   }
   async presentLoading() {
     this.loading = await this.loadingCtrl.create({ message: 'Por favor, espere...' });
     return this.loading.present();
   }
 
- 
+
 }
